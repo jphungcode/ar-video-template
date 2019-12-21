8362fd77512052bf1b28c8b928bba51251923275
johnphungtech <john.phung101@hotmail.com>
2019-12-22 00:43:33 +1100

    removed src folder

--- src/utils/compatibility.js
+++
@@ -1,7 +0,0 @@
-export default function(){
-    if(navigator.mediaDevices == undefined || navigator.mediaDevices.enumerateDevices == undefined || navigator.mediaDevices.getUserMedia == undefined){
-        return false;
-    }
-
-    return true
-}
